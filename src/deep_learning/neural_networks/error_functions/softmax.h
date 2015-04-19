93224ff27001ce169b32a9a76b8340532750ddf1
Tudor Berariu <tudor.berariu@gmail.com>
2015-04-19 09:52:12 +0300

    Added softmax and sum of squares error functions

---
+++ src/deep_learning/neural_networks/error_functions/softmax.h
@@ -0,0 +1,60 @@
+// Copyright (C) 2015 Tudor Berariu <tudor.berariu@gmail.com>
+
+#ifndef SOFTMAX_H
+#define SOFTMAX_H
+
+#include <cmath>
+#include <array>
+
+using namespace std;
+
+template<typename T>
+struct SoftMax {
+  static constexpr bool transforms_last_layer = true;
+  static constexpr bool supports_backpropagation = true;
+
+  template<typename LayerSize>
+  using Output = array<T, LayerSize::length>;
+
+  template<typename LayerSize, size_t batch_size>
+  using Outputs = array<Output<LayerSize>, batch_size>;
+
+  template<typename LayerSize, size_t batch_size>
+  inline static void
+  f(const Outputs<LayerSize, batch_size>& a,
+    Outputs<LayerSize, batch_size>& y) {
+    for (size_t n = 0; n < batch_size; n++) {
+      T sum = 0;
+      for (size_t i = 0; i < LayerSize::length; i++) {
+        y[n][i] = exp(a[n][i]);
+        sum += y[n][i];
+      }
+      for (size_t i = 0; i < LayerSize::length; i++) {
+        y[n][i] = y[n][i] / sum;
+      }
+    }
+  }
+
+  template<typename LayerSize, size_t batch_size>
+  inline static T
+  error(const Outputs<LayerSize, batch_size>& y,
+        const Outputs<LayerSize, batch_size>& t) {
+    T err = 0;
+    for (size_t n = 0; n < batch_size; n++)
+      for (size_t i = 0; i < LayerSize::length; i++)
+        err += t[n][i] * log(y[n][i]);
+    return err;
+  }
+
+  template<typename LayerSize, size_t batch_size>
+  inline static void
+  dError(const Outputs<LayerSize, batch_size>& y,
+         const Outputs<LayerSize, batch_size>& t,
+         Outputs<LayerSize, batch_size>& e) {
+    for (size_t n = 0; n < batch_size; n++)
+      for (size_t i = 0; i < LayerSize::length; i++)
+        e[n][i] = y[n][i] - t[n][i];
+  }
+};
+
+#endif
---
+++ src/deep_learning/neural_networks/error_functions/sum_of_squares.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2015 Tudor Berariu <tudor.berariu@gmail.com>
+
+#ifndef SUM_OF_SQUARES_H
+#define SUM_OF_SQUARES_H
+
+#include <cmath>
+#include <array>
+
+template<typename T>
+struct SumOfSquares {
+  static constexpr bool transforms_last_layer = false;
+  static constexpr bool supports_backpropagation = true;
+
+  template<typename LayerSize, size_t batch_size>
+  using Output = std::array<T, LayerSize::length>;
+
+  template<typename LayerSize, size_t batch_size>
+  using Outputs = std::array<Output<LayerSize, batch_size>, batch_size>;
+
+  template<typename LayerSize, size_t batch_size>
+  inline static T
+  error(const Outputs<LayerSize, batch_size>& y,
+        const Outputs<LayerSize, batch_size>& t) {
+    T err = 0;
+    for (size_t n = 0; n < batch_size; n++)
+      for (size_t i = 0; i < LayerSize::length; i++)
+        err += (t[n][i] - y[n][i]) * (t[n][i] - y[n][i]);
+    return err / (T)2;
+  }
+
+  template<typename LayerSize, size_t batch_size>
+  inline static void
+  dError(const Outputs<LayerSize, batch_size>& y,
+         const Outputs<LayerSize, batch_size>& t,
+         Outputs<LayerSize, batch_size>& e) {
+    for (size_t n = 0; n < batch_size; n++)
+      for (size_t i = 0; i < LayerSize::length; i++)
+        e[n][i] = y[n][i] - t[n][i];
+  }
+};
+
+#endif
