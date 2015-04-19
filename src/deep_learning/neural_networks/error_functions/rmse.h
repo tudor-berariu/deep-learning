fd3200cc36932473c72dbd7865e4fa9b8df87326
Tudor Berariu <tudor.berariu@gmail.com>
2015-04-19 15:38:41 +0300

    Minor optimization for RMSE: should be important for large batches

--- src/deep_learning/neural_networks/error_functions/rmse.h
+++ src/deep_learning/neural_networks/error_functions/rmse.h
@@ -23,22 +23,29 @@ struct RMSE {
   error(const Outputs<LayerSize, batch_size>& y,
         const Outputs<LayerSize, batch_size>& t) {
     Output<LayerSize> avg_label;
-    for (size_t i = 0; i < LayerSize::length; i++)
-      avg_label[i] = 0;
-    for (size_t n = 0; n < batch_size; n++)
+    for (T& avg_i : avg_label)
+      avg_i = 0;
+    for (size_t n = 0; n < batch_size; n++) {
+      const Output<LayerSize>* const t_row =
+        reinterpret_cast<const Output<LayerSize>*>(t[n].data());
       for (size_t i = 0; i < LayerSize::length; i++)
-        avg_label[i] += t[n][i];
-    for (size_t i = 0; i < LayerSize::length; i++)
-      avg_label[i] /= (T)batch_size;
+        avg_label[i] += (*t_row)[i];
+    }
+    for (T& avg_i : avg_label)
+      avg_i /= (T)batch_size;
 
     T err = (T)0;
     T norm = (T)0;
 
     for (size_t n = 0; n < batch_size; n++) {
-      for (size_t i = 0; i < LayerSize::length; i++)
-        err += (t[n][i] - y[n][i]) * (t[n][i] - y[n][i]);
-      for (size_t i = 0; i < LayerSize::length; i++)
-        norm += (t[n][i] - avg_label[i]) * (t[n][i] - avg_label[i]);
+      const Output<LayerSize>* const t_row =
+        reinterpret_cast<const Output<LayerSize>*>(t[n].data());
+      const Output<LayerSize>* const y_row =
+        reinterpret_cast<const Output<LayerSize>*>(y[n].data());
+      for (size_t i = 0; i < LayerSize::length; i++) {
+        err += ((*t_row)[i] - (*y_row)[i]) * ((*t_row)[i] - (*y_row)[i]);
+        norm += ((*t_row)[i] - avg_label[i]) * ((*t_row)[i] - avg_label[i]);
+      }
     }
 
     return (err / norm);
